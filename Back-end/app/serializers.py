220f67b2c4f7fb98b90b700691566fd65e6349f0
Nicolas Duarte <nickboytube0418@gmail.com>
2025-05-30 08:59:25 -0300

    Fazendo os serializers no serializers.py

---
+++ Back-end/app/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Sensors, Ambients, Historic
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'cargo']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+class SensorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensors
+        fields = '__all__'
+
+class AmbientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ambients
+        fields = '__all__'
+
+class HistoricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historic
+        fields = '__all__'
