083085a05b8cdbfa4ad6bdf70a7be15e3a0cb76c
Nicolas Duarte <nickboytube0418@gmail.com>
2025-05-30 08:52:49 -0300

    Adicionando informações para a página de administrador do próprio django

--- Back-end/app/admin.py
+++ Back-end/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Sensors, Ambients, Historic
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Novos campos', {'fields': ('cargo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('cargo')}),
+    )
+
+admin.site.register(User, UsuarioAdmin)
+admin.site.register(Sensors)
+admin.site.register(Ambients)
+admin.site.register(Historic)
