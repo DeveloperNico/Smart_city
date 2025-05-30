356fd5129aa628c25905b34f3f82502900669bb2
Nicolas Duarte <nickboytube0418@gmail.com>
2025-05-30 09:10:08 -0300

    Fazendo as urls para testar se está funcionando, talvez tenha muitos erros

---
+++ Back-end/app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserRetriveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
+    path('sensors/', SensorsListCreateView.as_view(), name='sensors-list-create'),
+    path('sensors/<int:pk>/', SensorsRetriveUpdateDestroyView.as_view(), name='sensors-retrieve-update-destroy'),
+    path('ambients/', AmbientsListCreateView.as_view(), name='ambients-list-create'),
+    path('ambients/<int:pk>/', AmbientsRetriveUpdateDestroyView.as_view(), name='ambients-retrieve-update-destroy'),
+    path('historic/', HistoricListCreateView.as_view(), name='historic-list-create'),
+    path('historic/<int:pk>/', HistoricRetriveUpdateDestroyView.as_view(), name='historic-retrieve-update-destroy'),
+]
