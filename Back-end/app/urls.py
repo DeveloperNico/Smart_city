4ee8601d94b9e769c6fb7117abdaf47e44f92874
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-03 10:03:06 -0300

    Ajustando os endpoints dos sensores

--- Back-end/app/urls.py
+++ Back-end/app/urls.py
@@ -5,9 +5,25 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserRetriveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
-    # path('import-sensors-excel/', importSensorsExcelView.as_view(), name='import-sensors-excel'),
-    # path('sensors/', SensorsListCreateView.as_view(), name='sensors-list-create'),
-    # path('sensors/<int:pk>/', SensorsRetriveUpdateDestroyView.as_view(), name='sensors-retrieve-update-destroy'),
+
+    # Endpoints para importação de sensores via Excel -------------------------------------------------------------------------------
+    path('sensors/counter/import/', importSensorsCounterExcelView.as_view(), name='import-sensors-counter-excel'),
+    path('sensors/temperature/import/', importSensorsTemperatureExcelView.as_view(), name='import-sensors-temperature-excel'),
+    path('sensors/humidity/import/', importSensorsHumidityExcelView.as_view(), name='import-sensors-humidity-excel'),
+    path('sensors/luminosity/import/', importSensorsLuminosityExcelView.as_view(), name='import-sensors-luminosity-excel'),
+    # -------------------------------------------------------------------------------------------------------------------------------
+    
+    # Importação de sensores de luminosidade, temperatura, umidade e contadores GET, POST, PUT, DELETE ------------------------------------------------
+    path('sensors/counter/', SensorCounterListCreateView.as_view(), name='sensor-counter-list-create'),
+    path('sensors/counter/<int:pk>/', SensorCounterRetriveUpdateDestroyView.as_view(), name='sensor-counter-retrieve-update-destroy'),
+    path('sensors/temperature/', SensorTemperatureListCreateView.as_view(), name='sensor-temperature-list-create'),
+    path('sensors/temperature/<int:pk>/', SensorTemperatureRetriveUpdateDestroyView.as_view(), name='sensor-temperature-retrieve-update-destroy'),
+    path('sensors/humidity/', SensorHumidityListCreateView.as_view(), name='sensor-humidity-list-create'),
+    path('sensors/humidity/<int:pk>/', SensorHumidityRetriveUpdateDestroyView.as_view(), name='sensor-humidity-retrieve-update-destroy'),
+    path('sensors/luminosity/', SensorLuminosityListCreateView.as_view(), name='sensor-luminosity-list-create'),
+    path('sensors/luminosity/<int:pk>/', SensorLuminosityRetriveUpdateDestroyView.as_view(), name='sensor-luminosity-retrieve-update-destroy'),
+    # --------------------------------------------------------------------------------------------------------------------------------------------------
+
     path('ambients/', AmbientsListCreateView.as_view(), name='ambients-list-create'),
     path('ambients/<int:pk>/', AmbientsRetriveUpdateDestroyView.as_view(), name='ambients-retrieve-update-destroy'),
     path('historic/', HistoricListCreateView.as_view(), name='historic-list-create'),
