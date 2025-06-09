fc052b3c69b0e60309f87387cef29331503e4930
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-09 11:01:46 -0300

    Teminando a página home

--- Front-end/src/Pages/PageIntro.jsx
+++ Front-end/src/Pages/PageIntro.jsx
@@ -1,11 +1,19 @@
+import styles from "./PageIntro.module.css";
 import { Background } from "../Components/Background/Background";
 import { Header } from "../Components/Header/Header";
+import ButtonStart  from "../Components/ButtonStart/ButtonStart"
 
 export function PageIntro() {
     return (
         <div>
             <Background />
             <Header />
+            <div className={styles.content}>
+                <h1>SMART FLOW</h1>
+                <p>“Welcome to the Smart Flow, the smart city where technology, sustainability and well-being <br />
+                    come together to transform the way we live, work and connect."</p>
+                <ButtonStart />
+            </div>
         </div>
     )
 }
