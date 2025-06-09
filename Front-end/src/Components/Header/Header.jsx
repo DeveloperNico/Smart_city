d99ba8a98e910afbe3d797762648450d61b26a8b
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-09 10:19:16 -0300

    Fazendo o header da página

--- Front-end/src/Components/Header/Header.jsx
+++ Front-end/src/Components/Header/Header.jsx
@@ -3,8 +3,13 @@ import Logo from '../../assets/Icons/Logo.svg';
 
 export function Header() {
     return (
-        <header>
+        <header className={styles.headerContainer}>
             <img src={Logo} alt="Minha logo" />
+            <div className={styles.linksHeader}>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Login</a>
+            </div>
         </header>
     )
 }
