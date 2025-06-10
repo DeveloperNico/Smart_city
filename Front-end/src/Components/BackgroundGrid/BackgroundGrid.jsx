66b1b19396db64f8c7b65e206d90df7bc32b0315
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-10 09:01:28 -0300

    ...

--- Front-end/src/Components/BackgroundGrid/BackgroundGrid.jsx
+++ Front-end/src/Components/BackgroundGrid/BackgroundGrid.jsx
@@ -1,14 +1,13 @@
 import styles from './BackgroundGrid.module.css';
-import testImage from '../../assets/Images/teste.png';
 
 export function BackgroundGrid() {
   const totalColumns = 4;
-  const itemsPerColumn = 4;
+  const itemsPerColumn = 10; // Aumentei para melhor visualização e para que a animação tenha mais "conteúdo"
 
   return (
-    <div className={styles.slider}>
+    <div className={styles.slider} style={{ '--quantity': itemsPerColumn }}>
       {Array.from({ length: totalColumns }).map((_, columnIndex) => {
-        const isReversed = columnIndex % 2 !== 0;
+        const isReversed = columnIndex % 2 !== 0; // Colunas ímpares terão animação reversa
 
         return (
           <div className={styles.column} key={columnIndex}>
@@ -20,7 +19,27 @@ export function BackgroundGrid() {
                   style={{ '--position': itemIndex + 1 }}
                 >
                   <div className={styles.skewFix}>
-                    <img src={testImage} alt="Grid item" />
+                    <div className={styles.container}>
+                      <div className={styles.content}>
+                        {/* Conteúdo dinâmico, se necessário. Ex: Item {itemIndex + 1} */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              {/* Duplicar os itens para simular um loop contínuo sem "gaps" */}
+              {Array.from({ length: itemsPerColumn }).map((_, itemIndex) => (
+                <div
+                  key={`duplicate-${itemIndex}`} // Chave única para o item duplicado
+                  className={styles.item}
+                  style={{ '--position': itemIndex + 1 }}
+                >
+                  <div className={styles.skewFix}>
+                    <div className={styles.container}>
+                      <div className={styles.content}>
+                        {/* Conteúdo dinâmico, se necessário. Ex: Item {itemIndex + 1} */}
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))}
--- Front-end/src/Pages/PageIntro.jsx
+++ Front-end/src/Pages/PageIntro.jsx
@@ -1,19 +1,13 @@
 import styles from "./PageIntro.module.css";
 import { Background } from "../Components/Background/Background";
 import { Header } from "../Components/Header/Header";
-import ButtonStart  from "../Components/ButtonStart/ButtonStart"
+import ButtonStart  from "../Components/ButtonStart/ButtonStart";
+import { BackgroundGrid } from "../Components/BackgroundGrid/BackgroundGrid";
 
 export function PageIntro() {
     return (
         <div>
-            <Background />
-            <Header />
-            <div className={styles.content}>
-                <h1>SMART FLOW</h1>
-                <p>“Welcome to the Smart Flow, the smart city where technology, sustainability and well-being <br />
-                    come together to transform the way we live, work and connect."</p>
-                <ButtonStart />
-            </div>
+            <BackgroundGrid />
         </div>
     )
 }
