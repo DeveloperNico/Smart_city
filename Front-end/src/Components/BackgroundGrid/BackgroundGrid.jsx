f9321ac99adfdb9e494c8f5bc46a7a9a69ebb4fc
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-10 10:21:16 -0300

    ...

--- Front-end/src/Components/BackgroundGrid/BackgroundGrid.jsx
+++ Front-end/src/Components/BackgroundGrid/BackgroundGrid.jsx
@@ -1,52 +1,22 @@
 import styles from './BackgroundGrid.module.css';
+import card from '../../assets/Icons/card.svg';
+
 
 export function BackgroundGrid() {
-  const totalColumns = 4;
-  const itemsPerColumn = 10; // Aumentei para melhor visualização e para que a animação tenha mais "conteúdo"
-
-  return (
-    <div className={styles.slider} style={{ '--quantity': itemsPerColumn }}>
-      {Array.from({ length: totalColumns }).map((_, columnIndex) => {
-        const isReversed = columnIndex % 2 !== 0; // Colunas ímpares terão animação reversa
-
-        return (
-          <div className={styles.column} key={columnIndex}>
-            <div className={`${styles.list} ${isReversed ? styles.reverse : ''}`}>
-              {Array.from({ length: itemsPerColumn }).map((_, itemIndex) => (
-                <div
-                  key={itemIndex}
-                  className={styles.item}
-                  style={{ '--position': itemIndex + 1 }}
-                >
-                  <div className={styles.skewFix}>
-                    <div className={styles.container}>
-                      <div className={styles.content}>
-                        {/* Conteúdo dinâmico, se necessário. Ex: Item {itemIndex + 1} */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-              {/* Duplicar os itens para simular um loop contínuo sem "gaps" */}
-              {Array.from({ length: itemsPerColumn }).map((_, itemIndex) => (
-                <div
-                  key={`duplicate-${itemIndex}`} // Chave única para o item duplicado
-                  className={styles.item}
-                  style={{ '--position': itemIndex + 1 }}
-                >
-                  <div className={styles.skewFix}>
-                    <div className={styles.container}>
-                      <div className={styles.content}>
-                        {/* Conteúdo dinâmico, se necessário. Ex: Item {itemIndex + 1} */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
+    return (
+        <div className={styles.slider}>
+            <div className={styles.list}>
+                <div className={styles.item} style={{'--position': 1}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 2}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 3}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 4}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 5}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 6}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 7}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 8}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 9}}><img src={card} alt="" /></div>
+                <div className={styles.item} style={{'--position': 10}}><img src={card} alt="" /></div>
             </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+        </div>
+    )
 }
