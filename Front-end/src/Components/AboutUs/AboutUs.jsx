e5e878d82441ded675204c718c8a58df37bcd1cd
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-15 18:20:22 -0300

    Colocando a parte de sobre nós

--- Front-end/src/Components/AboutUs/AboutUs.jsx
+++ Front-end/src/Components/AboutUs/AboutUs.jsx
@@ -1,57 +1,48 @@
 import styles from './AboutUs.module.css';
+import fotoPerfil from '../../assets/Images/foto-pessoal.jpeg';
+import LogoPreta from '../../assets/Icons/Logo-preta.svg';
+
 
 export function AboutUs() {
     return (
         <>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
-            <h1>Olá mundo!</h1>
+            <div className={styles.container}>
+                <div className={styles.aboutMe}>
+                    <figure>
+                        <img src={fotoPerfil} alt="Image for About Us" />
+                    </figure>
+
+                    <div>
+                        <div>
+                            <p>Hi, my name is Nicolas Duarte. I'm a developer with a passion for technology, always looking for innovative solutions that make a difference to people's daily lives.</p>
+
+                            <p>I have a dedicated, optimistic profile and believe in the power of technology to transform realities.</p>
+
+                            <p>Throughout my career, I have specialized in creating intelligent systems focused on efficiency, practicality and collective well-being.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.aboutSystem}>
+                    <h2>About</h2>
+                    <figure>
+                        <img src={LogoPreta} alt="" />
+                    </figure>
+
+                    <div>
+                        <p>
+                            The Smart Flow was designed to be the digital heart of a Smart City — a centralized platform that connects people, services, and technology in a fluid and intelligent way. 
+                            With a user-friendly interface and efficient integration, it allows citizens to access essential urban resources with just a few clicks.
+                        </p>
+
+                        <p>
+                            More than just a system, this platform is a step forward in building a more sustainable, connected, and inclusive city. 
+                            It reflects a commitment to innovation and social responsibility, making technology a true ally in transforming the future of urban living.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
