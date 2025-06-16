43115ba9c4201a3921b7c42a30b96785862e6852
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-16 20:50:27 -0300

    ...

--- Front-end/src/Components/ContentInfo/ContentInfo.jsx
+++ Front-end/src/Components/ContentInfo/ContentInfo.jsx
@@ -1,6 +1,7 @@
 import styles from './ContentInfo.module.css';
-import ButtonStart from '../ButtonStart/ButtonStart';
-import ButtonScroll from '../ButtonScroll/ButtonScroll';
+import ButtonStart from '../Components-Uiverse/ButtonStart/ButtonStart';
+import ButtonScroll from '../Components-Uiverse/ButtonScroll/ButtonScroll';
+import { NavLink }  from 'react-router-dom';
 
 export function ContentInfo() {
     const scrollToAbout = () => {
@@ -13,15 +14,16 @@ export function ContentInfo() {
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.oi}>
+                <div className={styles.contentPrincipal}>
                     <h1 className={styles.title}>SMART FLOW</h1>
                     <p className={styles.text}>“Welcome to the Smart Flow, the smart city where technology, sustainability and well-being <br />
                             come together to transform the way we live, work and connect."</p>
-                    <ButtonStart />    
+                    <NavLink to={"/login"} className={styles.navLink}>
+                        <ButtonStart />
+                    </NavLink> 
                 </div>
                 
                 <div className={styles.buttonScroll}>
-                    {/* Botão para scroll aqui */}
                     <ButtonScroll className={styles.scrollButton} onClick={scrollToAbout} />
                 </div>
             </div>
