327103a1c986ba6af8f6e7d72bd0349104ba33b8
Nicolas Duarte <nickboytube0418@gmail.com>
2025-06-20 01:52:05 -0300

    ...

--- Front-end/src/Components/ContentInfo/ContentInfo.jsx
+++ Front-end/src/Components/ContentInfo/ContentInfo.jsx
@@ -18,7 +18,7 @@ export function ContentInfo() {
                     <h1 className={styles.title}>SMART FLOW</h1>
                     <p className={styles.text}>“Welcome to the Smart Flow, the smart city where technology, sustainability and well-being <br />
                             come together to transform the way we live, work and connect."</p>
-                    <NavLink to={"/login"} className={styles.navLink}>
+                    <NavLink to={"/singUp"} className={styles.navLink}>
                         <ButtonStart />
                     </NavLink> 
                 </div>
